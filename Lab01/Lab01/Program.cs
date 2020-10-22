213478d29da6d311b661d7a70345a9bb8621fdfb
Mustafa <S01259464@acad.tri-c.edu>
2020-10-22 18:37:35 -0400

    Completed Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            // Tells C# to write this string in the console
+            Console.WriteLine("Hello World from Mustafa Muhammad");
+                     // Tells computer to wait after it is finished printing - (Let me read it then I will close it)
+            Console.ReadLine();
+        }
+    }
+}
