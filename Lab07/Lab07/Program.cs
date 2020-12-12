b0e4a52d0c2f904f99ab6100b243ee02e91784ed
Mustafa <S01259464@acad.tri-c.edu>
2020-12-11 21:22:28 -0500

    Completed Lab 07

---
+++ Lab07/Lab07/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book_1 = new Book();
+            book_1.Display();
+
+            BookTest book_2 = new BookTest("Friends Not Masters - A Political Autobiography", "Mohammad Ayub Khan", "1967");
+            book_2.display_2();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book
+    {
+        public string Title = "Friends Not Masters - A Political Autobiography";
+        public string Author = "Mohammad Ayub Khan";
+        public string Year = "1967";
+        public void Display()
+        {
+            Console.WriteLine($"Title: {Title}\nAuthor: {Author}\nYear: {Year}");
+            Console.WriteLine();
+        }
+    }
+
+    class BookTest
+    {
+        public string title;
+        public string author;
+        public string year;
+        public BookTest(string title, string author, string year)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+        public void display_2()
+        {
+            Console.WriteLine($"Title: {this.title}\nAuthor: {this.author}\nYear: {this.year}");
+        }
+
+    }
+}
